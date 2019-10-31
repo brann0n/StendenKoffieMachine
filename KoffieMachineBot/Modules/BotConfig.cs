d071252e45dd653bb6ea3dbeb27ff311e37d68e1
Brandon <43607839+brann0n@users.noreply.github.com>
2019-10-31 17:16:35 +0100

    Set up bot config in the program class, next step is command handling

---
+++ KoffieMachineBot/Modules/BotConfig.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace KoffieMachineBot.Modules
+{
+	class BotConfig
+	{
+		private const string ConfigFolder = "Resources"; //Folder in which you store the config files. (relative to project, but can be anywhere on the disk)
+		private const string ConfigFile = "config.json";
+
+		public static BotConfigProperties bot;
+
+		static BotConfig()
+		{
+			//check if the directory is present, otherwise create it.
+			if (!Directory.Exists(ConfigFolder))
+				Directory.CreateDirectory(ConfigFolder);
+
+			//check if the config file is present, otherwise create it.
+			if (!File.Exists(ConfigFolder + "/" + ConfigFile))
+			{
+				bot = new BotConfigProperties();
+				string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+				File.WriteAllText(ConfigFolder + "/" + ConfigFile, json);
+				Console.WriteLine("Config file was not present, creating file now.");
+			}
+			else
+			{
+				string json = File.ReadAllText(ConfigFolder + "/" + ConfigFile);
+				bot = JsonConvert.DeserializeObject<BotConfigProperties>(json);
+			}
+		}
+	}
+
+	public struct BotConfigProperties
+	{
+		public string Token;
+		public string CmdPrefix;
+	}
+}
--- KoffieMachineBot/Program.cs
+++ KoffieMachineBot/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,41 @@ namespace KoffieMachineBot
 {
 	class Program
 	{
-		static void Main(string[] args)
+		DiscordSocketClient _client;
+
+		static void Main(string[] args) => new Program().StartBotAsync().GetAwaiter().GetResult();
+
+
+		public async Task StartBotAsync()
 		{
+			//check if the config was loaded successfully
+			if (string.IsNullOrEmpty(Modules.BotConfig.bot.Token))
+			{
+				Console.WriteLine("No Authtoken present, please check the config file and restart the bot.");
+				return;
+			}
+
+			//Set log Level
+			_client = new DiscordSocketClient(new DiscordSocketConfig
+			{
+				LogLevel = LogSeverity.Verbose
+			});
+
+			//Assign client event triggers.
+			_client.Log += Log;
+
+			//Connect the bot to discord
+			await _client.LoginAsync(TokenType.Bot, Modules.BotConfig.bot.Token);
+			await _client.StartAsync();
+
+
+			//stay in this function forever
+			await Task.Delay(-1);
+		}
+
+		private async Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.Message);
 		}
 	}
 }
