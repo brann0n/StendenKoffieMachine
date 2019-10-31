5e2da09f93f009b5bebca37a2e2844c3df832d41
Brandon <43607839+brann0n@users.noreply.github.com>
2019-10-31 18:13:35 +0100

    Added owner shutdown command

---
+++ KoffieMachineBot/Commands/OwnerCommands.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoffieMachineBot.Commands
+{
+	[RequireOwner]
+	public class OwnerCommands : ModuleBase<SocketCommandContext>
+	{
+		[Command("Shutdown")]
+		public async Task ShutDown()
+		{
+			await ReplyAsync("Shutting down the bot");
+			System.Environment.Exit(1);
+		}
+	}
+}
